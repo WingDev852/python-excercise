d8044ba806facc4538c29aad4fa5ffe0f4b49b4a
Wing_Dev <wing_lai_dev@hotmail.com>
2026-04-15 16:56:41 +0200

    band name generator

---
+++ bandname generator/file.py
@@ -0,0 +1,7 @@
+print("This is your band name generator")
+
+city = input("What city do you live in?")
+
+name_of_pet = input("What is the name of your pet?")
+
+bandname = print("Your band name is " + city + " " + name_of_pet)
